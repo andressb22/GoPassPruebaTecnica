ca9bbc3cd29e6ded2d3a7cfdccc6754f5dd71090
andresfel22 <andres.sierra@indtechcol.com>
2026-04-27 15:55:28 -0500

    logOut

--- frontedProyectManager/src/components/Header.tsx
+++ frontedProyectManager/src/components/Header.tsx
@@ -1,14 +1,21 @@
 import { LogOut } from "lucide-react";
 import styles from "./styles/header.module.css"
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigation = useNavigate()
+  const logOut = () => {
+    sessionStorage.removeItem("token")
+    navigation("/")
+  }
+
   return (
     <div className={styles.container}>
       <div>
         <h1>Gestor de proyectos</h1>
       </div>
       <div>
-        <LogOut />
+        <LogOut onClick={logOut} />
       </div>
     </div>
   );
